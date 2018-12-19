bc02fc68daacda2b9b285ce7965e064fa1fecebc
Phil Hartley <philh725@gmail.com>
2018-12-19 05:18:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,8 @@ def all_holidays_with_bbq(holiday_hash)
   
   bbq_holidays = []
   holiday_hash.each do |season, holiday|
-    if holiday.include?()
+    if holiday.include?("BBQ")
+      bbq_holidays << holiday
 
 end
 
