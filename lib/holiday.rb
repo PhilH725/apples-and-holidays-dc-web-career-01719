d15a9fbe246dd4228049996898719d2db03d549e
Phil Hartley <philh725@gmail.com>
2018-12-19 06:11:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,8 +70,12 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     
     data.each do |holiday, supplies|
-      
-      puts "  #{holiday.capitalize}: #{supplies.join(', ')}"
+      semantics = holiday.split
+      semantics.each do |i|
+        i.capitalize
+      end
+      semantics.join(' ')
+      puts "  #{semantics}: #{supplies.join(', ')}"
       
     end
     
