96e137b87493bdf3f93610e887187a520d4e2b99
Phil Hartley <philh725@gmail.com>
2018-12-19 06:38:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday = holiday.to_s
       if holiday.include?('_')
         semantics = holiday.split('_')
-        semantics.each {|i| i.capitalize}
+        semantics = semantics.each {|i| i.capitalize}
         holiday = semantics.join(' ')
       end
       
