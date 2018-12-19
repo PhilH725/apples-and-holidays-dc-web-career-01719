8873bdb032d37aff673159569d886d780a115eb4
Phil Hartley <philh725@gmail.com>
2018-12-19 06:08:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_supplies_in_holidays(holiday_hash)
     
     data.each do |holiday, supplies|
       
-      puts "  #{holiday.capitalize}: #{supplies}"
+      puts "  #{holiday.capitalize}: #{supplies.join}"
       
     end
     
